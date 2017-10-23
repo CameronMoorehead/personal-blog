770c01a945d0cb3c45f82d21901ec1b562413122
Cameron <mooreheadc@hotmail.com>
2017-10-22 17:14:19 -0700

    Fixed viewport layouts for mobile

--- page.php
+++ page.php
@@ -7,7 +7,7 @@ get_header();
 if (have_posts()) :
     while (have_posts()) : the_post(); ?>
 
-    <article class="post page">
+    <article class="tester">
         <h2><?php the_title(); ?></h2>
         <?php the_content(); ?>
 
