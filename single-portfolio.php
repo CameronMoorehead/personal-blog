ace9b477834d45436b1e6a61aabac47c9d27e639
Cameron <mooreheadc@hotmail.com>
2017-11-22 15:17:27 -0800

    fixed various stylings

--- single-portfolio.php
+++ single-portfolio.php
@@ -7,7 +7,7 @@
 if (have_posts()) :
     while (have_posts()) : the_post(); ?>
 
-    <article class="post">
+    <article class="tester">
 
         <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
